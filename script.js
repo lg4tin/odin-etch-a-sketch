72c6d6bbbfd64aaa1e1b9f1491f337fdd7a5f27c
lg4tin <lg4tin@gmail.com>
2023-08-28 21:15:16 -0400

    Update createDivs function to allow a custom amount of rows and columns in the grid. Also add button to choose how many rows and cols.

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.prompt');
 
 function createDivs(num) {
+    let firstNum = num;
     num *= num;
 
     for (let i = 1; i <= num; i++) {
         let div = document.createElement('div');
-        div.style = 'flex: auto; background-color: red; width: 50px; height: 50px;';
-        div.innerText = i;
+        div.style = `flex: auto; background-color: grey; width: ${800 / firstNum}px; height: ${800 / firstNum}px;`;
+        //div.innerText = i;
         div.addEventListener('mouseenter', function() {
-            div.style.backgroundColor = 'blue';
+            div.style.backgroundColor = 'black';
             setTimeout(() => {
-                div.style.backgroundColor = 'red';
-            }, 1000)
+                div.style.backgroundColor = 'grey';
+            }, 2000)
         });
         container.appendChild(div);
     }
 }
 
-createDivs(16)
+button.addEventListener('click', function() {
+    let num = prompt('How many rows and columns?');
+    container.innerHTML = '';
+    createDivs(num)
+})
