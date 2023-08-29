f7a73a9ce0b534114ca43acdb16ca4b8d21451c1
lg4tin <lg4tin@gmail.com>
2023-08-29 19:35:41 -0400

    Add rgb Button and event listener to add rgb colors to the divs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('.prompt');
+const rgbButton = document.querySelector('.rgb');
 
 function createDivs(num) {
     let firstNum = num;
@@ -7,20 +8,35 @@ function createDivs(num) {
 
     for (let i = 1; i <= num; i++) {
         let div = document.createElement('div');
-        div.style = `flex: auto; background-color: grey; width: ${800 / firstNum}px; height: ${800 / firstNum}px;`;
-        //div.innerText = i;
+        div.style = `flex: auto; background-color: white; width: ${800 / firstNum}px; height: ${800 / firstNum}px;`;
+        div.classList.add('divs');
         div.addEventListener('mouseenter', function() {
             div.style.backgroundColor = 'black';
-            setTimeout(() => {
-                div.style.backgroundColor = 'grey';
-            }, 2000)
         });
         container.appendChild(div);
     }
 }
 
 button.addEventListener('click', function() {
-    let num = prompt('How many rows and columns?');
+    let num = prompt('How many rows and columns? Max: 100');
+    if (num > 100) {
+        num = 100;
+    } 
     container.innerHTML = '';
     createDivs(num)
 })
+
+rgbButton.addEventListener('click', function() {
+
+    let divs = document.querySelectorAll('.divs');
+
+    divs.forEach(div => {
+        div.addEventListener('mouseenter', function() {
+            let randomNumber1 = Math.floor((Math.random() * 255));
+            let randomNumber2 = Math.floor((Math.random() * 255));
+            let randomNumber3 = Math.floor((Math.random() * 255));
+            div.style.backgroundColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+        })
+    })
+})
+
